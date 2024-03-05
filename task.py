aee1088919a35cfb3139ab8d2450786735ab66b4
obich <g.shchet@gmail.com>
2024-03-05 16:34:38 +0300

    не понял как сделать текст двух разных размеров в hi

---
+++ task.py
@@ -0,0 +1,12 @@
+from flask import Flask, url_for, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/<title>')
+def index(title):
+    return render_template('base.html', title=title)
+
+
+if __name__ == '__main__':
+    app.run(port=8080, host='127.0.0.1')
