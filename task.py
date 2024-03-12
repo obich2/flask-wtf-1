7b8f0f36d1ef958673e5a31ef76f11ac68c1b1c5
George <g.shchet@gmail.com>
2024-03-12 23:05:48 +0300

    eventually i made it 1 hour of searching bug 0_0

--- task.py
+++ task.py
@@ -3,9 +3,19 @@ from flask import Flask, url_for, render_template
 app = Flask(__name__)
 
 
-@app.route('/<title>')
-def index(title):
-    return render_template('base.html', title=title)
+@app.route('/answer')
+def answer():
+    answer_dict = {
+        'title': 'Анкета',
+        'surname': 'Watny',
+        'name': 'Mark',
+        'education': 'выше среднего',
+        'profession': 'штурман марсохода',
+        'sex': 'male',
+        'motivation': 'Всегда мечтал застрять на Марсе!',
+        'ready': True
+    }
+    return render_template('index.html', **answer_dict)
 
 
 if __name__ == '__main__':
